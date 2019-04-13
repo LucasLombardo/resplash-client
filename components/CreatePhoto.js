063670e31cb5efced1317ea52e35fdf0841966a3
LucasLombardo <lucasalombardo@gmail.com>
2019-04-13 18:58:27 -0400

    finish create photo mutation

--- components/CreatePhoto.js
+++ components/CreatePhoto.js
@@ -1,33 +1,83 @@
 import React, { Component } from 'react';
-// import { Mutation } from 'react-apollo';
-// import gql from 'graphql-tag';
-// import Router from 'next/router';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Router from 'next/router';
 
-// eslint-disable-next-line
+const CREATE_PHOTO_MUTATION = gql`
+  mutation CREATE_PHOTO_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $photographer: String
+    $photographerLink: String
+    $thumbnail: String!
+    $largeImage: String!
+  ) {
+    createPhoto(data: {
+      title: $title
+      description: $description
+      price: $price
+      photographer: $photographer
+      photographerLink: $photographerLink
+      thumbnail: $thumbnail
+      largeImage: $largeImage
+    }) {
+      id
+    }
+  }
+`;
+
 class CreatePhoto extends Component {
+  state = { title: ``, price: ``, description: ``, photographer: ``, photographerLink: ``, thumbnail: `ddd`, largeImage: `` }
+
+  handleChange = (e) => {
+    const { name, type, value } = e.target;
+    const val = type === `number` ? parseFloat(value) : value;
+    this.setState({ [name]: val });
+  };
+
   render() {
+    const { title, price, description, photographer, photographerLink } = this.state;
+
     return (
-      <form>
-        <label htmlFor="title">Title
-          <input type="text" id="title" name="title" placeholder="Title of Photo" />
-        </label>
+      <Mutation mutation={CREATE_PHOTO_MUTATION} variables={this.state}>
+        {(createPhoto, { loading, error }) => (
+          <form onSubmit={async (e) => {
+            e.preventDefault();
+            const res = await createPhoto();
+            console.log(res);
+            Router.push({
+              pathname: `/`,
+            });
+          }}
+          >
+            {error && console.error(error)}
+            <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="title">Title
+                <input type="text" id="title" name="title" placeholder="Title of Photo" value={title} onChange={this.handleChange} />
+              </label>
 
-        <label htmlFor="price">Price
-          <input type="number" id="price" name="price" placeholder={100} />
-        </label>
+              <label htmlFor="price">Price
+                <input type="number" id="price" name="price" placeholder={100} value={price} onChange={this.handleChange} />
+              </label>
 
-        <label htmlFor="description">Description
-          <input type="text" id="description" name="description" placeholder="Description" />
-        </label>
+              <label htmlFor="description">Description
+                <input type="text" id="description" name="description" placeholder="Description" value={description} onChange={this.handleChange} />
+              </label>
 
-        <label htmlFor="photographer">Photographer
-          <input type="text" id="photographer" name="photographer" placeholder="Photographer Name" />
-        </label>
+              <label htmlFor="photographer">Photographer
+                <input type="text" id="photographer" name="photographer" placeholder="Photographer Name" value={photographer} onChange={this.handleChange} />
+              </label>
 
-        <label htmlFor="photographerLink">Link to Photographer&apos;s Website
-          <input type="text" id="photographerLink" name="photographerLink" placeholder="https://example.com" />
-        </label>
-      </form>
+              <label htmlFor="photographerLink">Link to Photographer&apos;s Website
+                <input type="text" id="photographerLink" name="photographerLink" placeholder="https://example.com" value={photographerLink} onChange={this.handleChange} />
+              </label>
+
+              <button type="submit">Submit</button>
+            </fieldset>
+          </form>
+        )}
+      </Mutation>
     );
   }
 }
