a24e06cab79e54d4f0efcc20aee69dbc8644447b
LucasLombardo <lucasalombardo@gmail.com>
2019-04-13 16:53:15 -0400

    add baseline layout styling

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Meta } from './Meta';
 import { Nav } from './Nav';
 import { GlobalStyles } from '../lib';
+
+const Main = styled.main`
+  width: 80%;
+  max-width: 1024px;
+  min-width: 300px;
+  margin: 0 auto;
+`;
 
 export const Layout = ({ children }) => (
   <>
     <Meta />
     <GlobalStyles />
     <header><Nav /></header>
-    <main>{children}</main>
+    <Main>{children}</Main>
   </>
 );
