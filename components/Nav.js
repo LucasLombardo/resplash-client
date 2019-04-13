ca885a1c84ab5e862f1bbeaa2247deeb77f809f1
LucasLombardo <lucasalombardo@gmail.com>
2019-04-13 16:52:49 -0400

    add styling for nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const NavBar = styled.nav`
+  display: flex;
+  border-bottom: 1px solid black;
+  justify-content: center;
+`;
+
+const NavItem = styled.a`
+    margin: .8em 2em 1em;
+    padding: 3px;
+    border: 1px solid rgba(0,0,0,0);
+    color: black;
+    text-decoration: none;
+    font-size: 1.1em;
+    font-weight: bold;
+    text-transform: uppercase;
+    cursor: pointer;
+
+    &:hover {
+      border: 1px solid black;
+    }
+`;
 
 export const Nav = () => (
-  <nav>
-    <Link href="/"><a>Home</a></Link>
-    <Link href="/page-2"><a>Page 2</a></Link>
-  </nav>
+  <NavBar>
+    <Link href="/"><NavItem>Home</NavItem></Link>
+    <Link href="/page-2"><NavItem>Page 2</NavItem></Link>
+  </NavBar>
 );
