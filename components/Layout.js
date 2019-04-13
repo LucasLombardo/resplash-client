847461b2fdea1b1a0835e2ee0a14cc608f4c3c7b
LucasLombardo <lucasalombardo@gmail.com>
2019-04-13 15:12:15 -0400

    add styled-components

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Meta } from './Meta';
 import { Nav } from './Nav';
+import { GlobalStyles } from '../lib';
 
 export const Layout = ({ children }) => (
   <>
     <Meta />
+    <GlobalStyles />
     <header><Nav /></header>
     <main>{children}</main>
   </>
