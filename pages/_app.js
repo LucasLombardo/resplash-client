31d78e0c435c6a0e914037bea5877cc49a2a10a2
LucasLombardo <lucasalombardo@gmail.com>
2019-04-13 13:51:53 -0400

    add Apollo Client to connect to backend
    Connect to the backend through Apollo Client
    - Add config file and store db endpoint in it
    - Add apollo packages and next-with-apollo package
    - Wrap custom _app.js with the apollo provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import App, { Container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
 import { Layout } from '../components';
+import { withData } from '../lib';
 
 class CustomApp extends App {
+    // fetch the data before rendering the pages, necessary for SSR
+    static async getInitialProps({ Component, ctx }) {
+        let pageProps = {};
+        // if the component being rendered has props, surface the
+        // page props from the page context
+        if (Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        pageProps.query = ctx.query;
+        // exposes the query to the client
+        return { pageProps };
+    }
+
     render() {
-        const { Component } = this.props;
+        const { Component, apollo, pageProps } = this.props;
 
         return (
             <Container>
-                <Layout>
-                    <Component />
-                </Layout>
+                <ApolloProvider client={apollo}>
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </ApolloProvider>
             </Container>
         );
     }
 }
 
-export default CustomApp;
+export default withData(CustomApp);
