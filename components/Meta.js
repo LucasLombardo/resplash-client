20509eabea088c23c4a97ec8c49a20648c0721bd
LucasLombardo <lucasalombardo@gmail.com>
2019-04-14 13:42:46 -0400

    add favicon

--- components/Meta.js
+++ components/Meta.js
@@ -6,5 +6,6 @@ export const Meta = () => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <title>Resplash</title>
+    <link rel="shortcut icon" href="https://res.cloudinary.com/dov1pamgz/image/upload/v1555263715/static/favicon.png" />
   </Head>
 );
