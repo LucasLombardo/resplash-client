a83ee58b11f5c78d62d3dd85fd9662dace855f7c
LucasLombardo <lucasalombardo@gmail.com>
2019-04-13 21:34:44 -0400

    add logic for image uploads

--- components/CreatePhoto.js
+++ components/CreatePhoto.js
@@ -29,7 +29,8 @@ const CREATE_PHOTO_MUTATION = gql`
 `;
 
 class CreatePhoto extends Component {
-  state = { title: ``, price: ``, description: ``, photographer: ``, photographerLink: ``, thumbnail: `ddd`, largeImage: `` }
+  // eslint-disable-next-line
+  state = { image: ``, title: ``, price: ``, description: ``, photographer: ``, photographerLink: ``, thumbnail: ``, largeImage: `` }
 
   handleChange = (e) => {
     const { name, type, value } = e.target;
@@ -37,8 +38,29 @@ class CreatePhoto extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async (e) => {
+    console.log(`uploading file...`);
+    const { files } = e.target;
+    if (files.length) {
+      const data = new FormData();
+      data.append(`file`, files[0]);
+      data.append(`upload_preset`, `resplash`);
+
+      const res = await fetch(`https://api.cloudinary.com/v1_1/dov1pamgz/image/upload`, {
+        method: `POST`,
+        body: data,
+      });
+      const file = await res.json();
+      // eslint-disable-next-line
+      this.setState({ thumbnail: file.secure_url, largeImage: file.eager[0].secure_url });
+    } else {
+      // eslint-disable-next-line
+      this.setState({ thumbnail: ``, largeImage: `` });
+    }
+  };
+
   render() {
-    const { title, price, description, photographer, photographerLink } = this.state;
+    const { title, price, description, photographer, photographerLink, thumbnail } = this.state;
 
     return (
       <Mutation mutation={CREATE_PHOTO_MUTATION} variables={this.state}>
@@ -54,6 +76,21 @@ class CreatePhoto extends Component {
           >
             {error && console.error(error)}
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+              </label>
+              {thumbnail && (
+                <img src={thumbnail} alt="Upload Preview" />
+              )}
+
               <label htmlFor="title">Title
                 <input type="text" id="title" name="title" placeholder="Title of Photo" value={title} onChange={this.handleChange} />
               </label>
--- components/Form.js
+++ components/Form.js
@@ -4,6 +4,12 @@ export const Form = styled.form`
     max-width: 600px;
     margin: 0 auto;
     font-size: 1.1em;
+    img {
+        display: block;
+        max-width: 240px;
+        border: 1px solid black;
+        margin: 0 auto;
+    }
     input {
         width: 100%;
         display: block;
