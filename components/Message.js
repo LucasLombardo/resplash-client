b250d671fb71a0667e4b5d7663b878418e235a0d
LucasLombardo <lucasalombardo@gmail.com>
2019-04-16 09:18:16 -0400

    add message component

---
+++ components/Message.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+// takes a type of error or success
+
+const MessageWrapper = styled.div`
+  background: lightcoral;
+  padding: 0.5em 1em;
+  border-left: 5px solid red;
+  border-right: 3px solid red;
+  margin: 1em 0;
+  position: relative;
+  button {
+    position: absolute;
+    right: 5px;
+    margin-top: -0.4em;
+    height: 30px;
+    width: 30px;
+    border: none;
+    background: rgba(255,255,255,0.4);
+    font-weight: bold;
+  }
+  &.success {
+    background: lightgreen;
+    border-left: 5px solid green;
+    border-right: 3px solid green;
+  }
+  &.closed {
+    display: none;
+  }
+`;
+
+export const Message = ({ type, children }) => {
+  const [messageClass, setMessageClass] = useState(type);
+
+  return (
+    <MessageWrapper className={messageClass}>
+      {children}
+      <button
+        type="button"
+        onClick={() => setMessageClass(`closed`)}
+      >
+        ✕
+      </button>
+    </MessageWrapper>
+  );
+};
