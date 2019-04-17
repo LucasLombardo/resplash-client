f15c83517c4e69b4f15bef221aaf99b3b309d7c4
LucasLombardo <lucasalombardo@gmail.com>
2019-04-17 10:14:30 -0400

    add pagination with a load more button

--- components/Photos.js
+++ components/Photos.js
@@ -2,28 +2,58 @@ import React from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import { PhotoCard } from './PhotoCard';
+import { Message } from './Message';
 
 export const ALL_PHOTOS_QUERY = gql`
-  query ALL_PHOTOS_QUERY {
-    photos {
+  query ALL_PHOTOS_QUERY($skip: Int) {
+    photos(orderBy: createdAt_DESC, first: 3, skip: $skip) {
       id
       title
       price
       thumbnail
       description
     }
+    photosConnection {
+      aggregate {
+        count
+      }
+    }
   }
 `;
 
+
 export const Photos = () => (
   <Query query={ALL_PHOTOS_QUERY}>
-    {({ data, error, loading }) => {
+    {({ data, error, loading, fetchMore }) => {
       if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error: {error.message}</p>;
+      if (error) return <Message error={error} />;
+      const { photos, photosConnection } = data;
+      const hasMore = photos.length < photosConnection.aggregate.count;
       return (
-        data.photos.map(photo => (
-          <PhotoCard photo={photo} key={photo.id} />
-        ))
+        <>
+          { data.photos.map(photo => (
+            <PhotoCard photo={photo} key={photo.id} />
+          ))}
+          { hasMore && (
+            <button
+              type="button"
+              onClick={() => fetchMore({
+                variables: {
+                  skip: photos.length
+                },
+                updateQuery: (prev, { fetchMoreResult }) => {
+                  if (!fetchMoreResult) return prev;
+                  return Object.assign({}, prev, {
+                    photos: [...prev.photos, ...fetchMoreResult.photos]
+                  });
+                }
+              })
+              }
+            >
+                Load More
+            </button>
+          )}
+        </>
       );
     }}
   </Query>
