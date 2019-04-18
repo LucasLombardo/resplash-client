f4ca0cbc86a9d9807d9b9576ae054b79023f78dd
LucasLombardo <lucasalombardo@gmail.com>
2019-04-18 19:44:55 -0400

    switch landing page to index

---
+++ pages/all.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Photos, Container } from '../components';
+
+const Home = () => (
+  <Container>
+    <h2>All Photos</h2>
+    <Photos />
+  </Container>
+);
+
+export default Home;
