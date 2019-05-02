bf54da43a1defd5e2d21b267d69cf4dc4a9c2ab1
LucasLombardo <lucasalombardo@gmail.com>
2019-05-02 11:56:22 -0400

    add featured links

--- components/LandingFeatured.js
+++ components/LandingFeatured.js
@@ -1,12 +1,9 @@
 import React from 'react';
+import Link from 'next/link';
 import styled from 'styled-components';
 import { Container } from './Container';
 
 const Featured = styled.div`
-    /* background: linear-gradient(
-      rgba(237,58,83, 0.45), 
-      rgba(237,58,83, 0.45)
-    ), url('https://res.cloudinary.com/dov1pamgz/image/upload/v1555602146/ep_naturalwhite.png'); */
     background: linear-gradient(
       rgba(255,255,255, 0.8), 
       rgba(255,255,255, 0.8)
@@ -29,13 +26,18 @@ const Featured = styled.div`
       background: white;
       border: 2px solid gray;
       height: 20vw;
-      background: url('https://res.cloudinary.com/dov1pamgz/image/upload/v1547317062/sample.jpg') no-repeat center;
+      background: url('https://res.cloudinary.com/dov1pamgz/image/upload/v1555637950/resplash/otgjtvgahn5xm4muoeyd.jpg') no-repeat center;
       background-size: cover;
+      cursor: pointer;
+      transition: 0.2s ease;
       &:nth-of-type(2) {
-        background: url('https://res.cloudinary.com/dov1pamgz/image/upload/v1547316989/road-to-zion-by-tyson-dudley.jpg') no-repeat center;
+        background: url('https://res.cloudinary.com/dov1pamgz/image/upload/v1555641985/resplash/fpyoaurluq0stc3kv1xk.jpg') no-repeat center;
       }
       &:nth-of-type(1) {
-        background: url('https://res.cloudinary.com/dov1pamgz/image/upload/v1555209794/iceland-right.jpg') no-repeat center;
+        background: url('https://res.cloudinary.com/dov1pamgz/image/upload/v1556743048/resplash/fr3o5es05wkqwwjnms8n.jpg') no-repeat center;
+      }
+      &:hover {
+        transform: scale(1.03) translateY(-3px);
       }
     }
 `;
@@ -45,9 +47,18 @@ export const LandingFeatured = () => (
     <Container>
       <h2>Featured Images</h2>
       <div className="featured">
-        <div className="featured-item" />
-        <div className="featured-item" />
-        <div className="featured-item" />
+        <Link href={{ pathname: `/photo`, query: { id: `cjv5omvz400cb07309592c0lb` } }}>
+          {/* Valley */}
+          <div className="featured-item" />
+        </Link>
+        <Link href={{ pathname: `/photo`, query: { id: `cjunh35lp03wt0767mzrzp69k` } }}>
+          {/* Paint Splat */}
+          <div className="featured-item" />
+        </Link>
+        <Link href={{ pathname: `/photo`, query: { id: `cjuneoykb03su0767yntkbc73` } }}>
+          {/* Sitting Woman */}
+          <div className="featured-item" />
+        </Link>
       </div>
     </Container>
   </Featured>
